df15c729b70f8d2c6e0011c812d815888ef5e5b4
Dustin Walker <thewalkman1234@gmail.com>
2017-06-05 15:45:19 -0500

    class work

---
+++ sonnet.js
@@ -0,0 +1,42 @@
+console.log("Hello World");
+//Take the contents of the sonnet div and place it in a variable.
+
+const sonnet = document.getElementById('sonnet');
+
+let sonnetText = sonnet.innerHTML
+
+console.log('sonnet', sonnetText);
+
+
+//Find and output the starting position of the word "orphans".
+
+var orphanIndex = sonnetText.indexOf("orphans");
+
+
+
+//Output the number of characters in the sonnet.
+
+var sonnetLength = sonnetText.length
+console.log("sonnet length", sonnetLength);
+
+
+
+//Replace the first occurance of the string "winter" with "yuletide".
+
+var sonnetUpdate = sonnetText.replace("winter","yeletide")
+console.log("changed Text", sonnetUpdate);
+
+
+
+
+//Replace all occurances of the string "the" with "a large".
+
+sonnetUpdate = sonnetUpdate.replace(/ the /g, "a large");
+console.log("sonnetUpdate", sonnetUpdate);
+
+
+
+
+//Set the content of the sonnet div with the new string.
+
+sonnetUpdate.innerHTML = sonnetUpdate;
